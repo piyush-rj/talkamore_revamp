fe5df75dd821f5cb1177a4a3efb3899b51ea78ce
kant-github <kantrishi7779@gmail.com>
2026-05-09 17:59:09 +0530

    added fonts prettier

---
+++ src/components/LandingBookSplitSection.tsx
@@ -0,0 +1,9 @@
+import { JSX } from "react";
+
+export default function LandingBookSplitSection(): JSX.Element {
+    return (
+        <main>
+            <section>sjbdvs</section>
+        </main>
+    );
+}
